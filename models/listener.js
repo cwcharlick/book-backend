5f120d20bcc15ebf6d294b808ec5e916af6d61ab
Chris Charlick <cwcharlick@outlook.com>
2021-09-20 16:39:38 +0100

    [Update] listener log out set to 48h

--- models/listener.js
+++ models/listener.js
@@ -29,7 +29,7 @@ async function expireListeners() {
 
   await Listener.deleteMany({
     lastCheckIn: {
-      $lt: new Date(new Date().getTime() - 240 * 60000).toISOString(),
+      $lt: new Date(new Date().getTime() - 48 * 60 * 60 * 1000).toISOString(),
     },
   });
 }
