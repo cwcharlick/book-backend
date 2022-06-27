915f2ef57047ba7a129d345f51ca87ff847f4a65
Chris Charlick <cwcharlick@outlook.com>
2022-06-27 21:00:57 +0100

    [Bugfix] Restaurant now added to schema for BookingLog

--- models/bookingLog.js
+++ models/bookingLog.js
@@ -6,6 +6,7 @@ const ObjectId = Schema.ObjectId;
 
 const bookingLogSchema = new Schema({
   booking: { type: ObjectId, ref: 'Booking', required: true },
+  restaurant: { type: ObjectId, ref: 'Restaurant', required: true },
   initials: { type: String, default: '' },
   value: {},
 });
@@ -15,6 +16,7 @@ const BookingLog = mongoose.model('BookingLog', bookingLogSchema);
 function validateBookingLog(bookingLog) {
   const schema = Joi.object({
     booking: Joi.objectId(),
+    restaurant: Joi.objectId(),
     initials: Joi.String(),
     id: Joi.allow(),
     _id: Joi.allow(),
