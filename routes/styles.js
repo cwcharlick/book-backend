141f8a510d94ef1217b0f0326054b888368b4d2f
Chris Charlick <cwcharlick@outlook.com>
2022-07-10 18:28:25 +0100

    [Feature] add super admin post styles route

--- routes/styles.js
+++ routes/styles.js
@@ -1,4 +1,5 @@
 const auth = require('../middleware/auth');
+const superAdmin = require('../middleware/superAdmin');
 const { Style, validateStyle } = require('../models/style.js');
 const express = require('express');
 const router = express.Router();
@@ -11,6 +12,28 @@ router.get(
       restaurant: req.params.restId,
     });
     res.send(styles);
+  })
+);
+
+router.post(
+  '/',
+  [auth, superAdmin],
+  addTryCatch(async (req, res) => {
+    const { error } = validateStyle(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const style = new Style({
+      restaurant: req.user.selectedRestaurant._id,
+      logoUrl: req.body.logoUrl,
+      logoStyle: req.body.logoStyle,
+      homeUrl: req.body.homeUrl,
+      colors: req.body.colors,
+      phone: req.body.phone,
+    });
+
+    await style.save();
+
+    res.send(style);
   })
 );
 
